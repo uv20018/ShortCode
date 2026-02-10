eb7d5742a4f4b3e3bc5e587a8b8db2b045d970e8
uv20018 <uditiverma2004.sis@gmail.com>
2026-02-10 22:32:38 +0530

    Changes saved

--- first.py
+++ first.py
@@ -2,3 +2,22 @@ n=5
 for i in range(n):
     print(" "*(n-i+1)+(2*i+1)*"*"+" "*(n-i+1),end="")
     print()
+
+
+i=5
+while i>0:
+    j=1
+    while j<=i:
+        print(j,end="")
+        j+=1
+    i-=1
+    print()
+
+
+    for i in range(5,0,-1):
+        for j in range(i,0,-1):
+            print("*",end="")
+        print()
+        
+
+        
